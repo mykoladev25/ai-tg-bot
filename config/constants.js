22093c23a59abcefa18e4e4e4bd1438533db9d3a
Nik Cherevan <mykola.cherevan@wam.global>
2026-01-26 18:50:53 +0100

    landing dynamic content

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,7 @@ const models = require('./models');
 
 const TRIAL_TOKENS = models.subscriptions?.trial?.tokens;
 const WORST_CASE_TOKEN_USD = models._pricingAssumptions?.worstCaseTokenUSD;
-const DEFAULT_ALERT_FAIL_RATE_PCT = 15;
+const DEFAULT_ALERT_FAIL_RATE_PCT = 10;
 
 module.exports = {
   TRIAL_TOKENS,
